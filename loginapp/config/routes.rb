68839ac24f5149a3bc000b72c2160fe3febb75da
Beatriz Santos <bsantosb@snu.ac.kr>
2015-04-08 10:22:16 +0900

    Users commit

--- loginapp/config/routes.rb
+++ loginapp/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
+  resources :users
+
+  get '/users/new'
+  root 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
