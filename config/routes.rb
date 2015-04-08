c94c8c607a7e2a340796ea631114e56389e6feb5
Beatriz Santos <bsantosb@snu.ac.kr>
2015-04-08 11:49:20 +0900

    first commits

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
+
+
+  resources :users
+
+
+  #root             'static_pages#home'
+  #get 'help'    => 'static_pages#help'
+  #get 'about'   => 'static_pages#about'
+  #get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
